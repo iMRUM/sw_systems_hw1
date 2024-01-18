7b6e98c067f5ed7c25b48715740b9a3ebf694462
imry <rodkin.imry@gmail.com>
2024-01-18 13:50:31 +0200

    constants are defined in NumClass.h isPrime(int n) is done in basicClassification.c

--- NumClass.h
+++ NumClass.h
@@ -1,8 +1,19 @@
-//
-// Created by rodki on 18/01/2024.
-//
+#define TRUE 1
+#define FALSE 0
+#define ZERO 0
+#define TWO 2
+/* will return if a number is Armstrong number
+An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+*/
+int isArmstrong(int n);
+/* will return if a number is a palindrome */
+int isPalindrome(int n);
+/* will return if a number is prime*/
+int isPrime(int n);
+/*Strong number is a special number whose sum of the factorial of digits is equal to the original
+number. For Example: 145 is strong number
+1! + 4! + 5! = 145
+*/
+int isStrong(int n);
 
-#ifndef SW_SYSTEMS_HW1_NUMCLASS_H
-#define SW_SYSTEMS_HW1_NUMCLASS_H
-
-#endif //SW_SYSTEMS_HW1_NUMCLASS_H
--- basicClassification.c
+++ basicClassification.c
@@ -1,3 +1,19 @@
-//
-// Created by rodki on 18/01/2024.
-//
+#include "NumClass.h"
+#include <math.h>
+
+int sqrt_number;
+int temp_factorial;
+int i;
+int isPrime(int n){
+    sqrt_number=(int) floor(sqrt(n));
+    for (i = TWO; i < sqrt_number; i++) {
+        if(n % i == ZERO){
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+//will use shift left operator for dividing, <<5 is n/10 (5 shifts left)
+int isStrong(int n){
+    return FALSE;
+}
