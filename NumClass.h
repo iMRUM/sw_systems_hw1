bcf9de8aca308addf494f3a2869c351570a594f2
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-01-22 15:38:38 +0200

    Add files via upload

--- NumClass.h
+++ NumClass.h
@@ -1,19 +1,15 @@
-#define TRUE 1
-#define FALSE 0
-#define ZERO 0
-#define TWO 2
-/* will return if a number is Armstrong number
-An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
-For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
-*/
-int isArmstrong(int n);
-/* will return if a number is a palindrome */
-int isPalindrome(int n);
-/* will return if a number is prime*/
-int isPrime(int n);
-/*Strong number is a special number whose sum of the factorial of digits is equal to the original
-number. For Example: 145 is strong number
-1! + 4! + 5! = 145
-*/
-int isStrong(int n);
+#ifndef NumClass_H
+#define NumClass_H
 
+#define false 0
+#define true 1
+int Flip(int a);
+double sqrt(double a);
+int isArmstrong(int a);
+int isArmstrongHelp(int a,int digit,int compare);
+int isPalindrome(int a);
+int isPrime(int a);
+int isStrong(int a);
+int countdigits(int a);
+int factorial(int a);
+#endif
