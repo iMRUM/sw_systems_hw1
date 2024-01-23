91ed4e0b1cbf83a7767bb02e0279e527c7e03657
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-01-23 13:17:19 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,27 @@
  #include "NumClass.h"
 #include <stdio.h>
 int main(){
-    double sq=9;
-    int a=37;
-    int p=1234321;
-    int k=407;
-    double t=sqrt(sq);
-    printf("%f\n",t);
-    if(isStrong(a)){
-        printf("%d is a strong number.\n",a);
-    }else{
-        printf("%d is not a strong number.\n",a);
-    }
-
-     if(isPrime(a)){
-        printf("%d is a prime number.\n",a);
-    }else{
-        printf("%d is not a prime number.\n",a);
-    }
-
-     if(isPalindrome(p)){
-        printf("%d is a Palindrome number.\n",p);
-    }else{
-        printf("%d is not a Palindrome number.\n",p);
-    }
-    if(isArmstrong(k)){
-        printf("%d is a Armstrong number.\n",k);
-    }else{
-        printf("%d is not a Armstrong number.\n",k);
-    }
-return 0;
+  int number1;
+  int number2;
+  int max;
+  int min;
+  scanf("%d %d" ,&number1,&number2);
+  if(number1>=number2){
+  max=number1; min=number2;
+  }else  max=number2; min=number1;
+for(int i=min+1; i<max; i++){
+    printf("%d",isPrime(i));
+}
+printf("\n");
+for(int i=min+1; i<max; i++){
+    printf("%d",isArmstrong(i));
+}
+printf("\n");
+for(int i=min+1; i<max; i++){
+ printf("%d",isStrong(i));
+}
+printf("\n");
+for(int i=min+1; i<max; i++){
+ printf("%d",isPalindrome);
 } 
- 
+}
