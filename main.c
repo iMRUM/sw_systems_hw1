b7aaaf3e10654e3e64ff0662bb76502d003a2eb6
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-01-23 13:55:26 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "NumClass.h"
+ #include "NumClass.h"
 #include <stdio.h>
 int main(){
   int number1;
@@ -7,11 +7,9 @@ int main(){
   int min;
   scanf("%d %d" ,&number1,&number2);
   if(number1>=number2){
-  	max=number1; 
-  	min=number2;
+  max=number1; min=number2;
   }else{
-  	max=number2; 
-  	min=number1;
+ max=number2; min=number1;
   }
 for(int i=min+1; i<max; i++){
     printf("%d",isPrime(i));
@@ -27,6 +25,5 @@ for(int i=min+1; i<max; i++){
 printf("\n");
 for(int i=min+1; i<max; i++){
  printf("%d",isPalindrome(i));
-}
-return 0; 
+} 
 }
