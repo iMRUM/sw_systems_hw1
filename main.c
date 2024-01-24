c6efe3ea32a9cca03b66c44c0ca09e30a35bc54b
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-01-24 19:39:15 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -11,31 +11,31 @@ int main(){
   }else{
  max=number2; min=number1;
   }
-   printf("The Armstrong numbers are: ");
+   printf("The Armstrong numbers are:");
 for(int i=min; i<max; i++){
     if(isArmstrong(i)){
-    printf("%d ",i);
+    printf(" %d",i);
     }
 }
 printf("\n");
- printf("The Palindromes are: ");
+ printf("The Palindromes are:");
 for(int i=min; i<max; i++){
     if(isPalindrome(i)){
- printf("%d  ",i);
+      printf(" %d",i);
     }
 } 
 printf("\n");
-  printf("The Prime numbers are: ");
+  printf("The Prime numbers are:");
 for(int i=min; i<max; i++){
     if(isPrime(i)){
-    printf("%d ",i );
+    printf(" %d",i );
     }
 }
 printf("\n");
-  printf("The Strong numbers are: ");
+  printf("The Strong numbers are:");
 for(int i=min; i<max; i++){
     if(isStrong(i)){
- printf("%d ",i);
+ printf(" %d",i);
     }
 }
  
